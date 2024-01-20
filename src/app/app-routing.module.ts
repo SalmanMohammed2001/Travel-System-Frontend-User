64556cc649fbb1fd182c1268470c5f8377080102
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-20 21:53:00 +0530

    create starter header and footer components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 
 const routes: Routes = [
-
+  {path:'',redirectTo:'starter',pathMatch:'full'},
   { path: 'console', loadChildren: () => import('./module/console/console.module').then(m => m.ConsoleModule) },
-
   { path: 'security', loadChildren: () => import('./module/security/security.module').then(m => m.SecurityModule) },
-
   { path: 'shared', loadChildren: () => import('./module/shared/shared.module').then(m => m.SharedModule) },
-
-  { path: 'starter', loadChildren: () => import('./module/starter/starter.module').then(m => m.StarterModule) }
+  { path: 'starter', loadChildren: () => import('./module/starter/starter.module').then(m => m.StarterModule) },
+  {path:'**',component:NotFoundPageComponent}
 ];
 
 @NgModule({
--- src/app/module/starter/starter.module.ts
+++ src/app/module/starter/starter.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { StarterRoutingModule } from './starter-routing.module';
 import { StarterComponent } from './starter.component';
+import { StarterHeaderComponent } from './components/starter-header/starter-header.component';
+import { StarterFooterComponent } from './components/starter-footer/starter-footer.component';
 
 
 @NgModule({
   declarations: [
-    StarterComponent
+    StarterComponent,
+    StarterHeaderComponent,
+    StarterFooterComponent
   ],
   imports: [
     CommonModule,
