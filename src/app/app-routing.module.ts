a0fc259fc0e86d0e5a6956c4f47cab7912647c21
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-20 21:43:01 +0530

    create main modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'console', loadChildren: () => import('./module/console/console.module').then(m => m.ConsoleModule) },
+
+  { path: 'security', loadChildren: () => import('./module/security/security.module').then(m => m.SecurityModule) },
+
+  { path: 'shared', loadChildren: () => import('./module/shared/shared.module').then(m => m.SharedModule) },
+
+  { path: 'starter', loadChildren: () => import('./module/starter/starter.module').then(m => m.StarterModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
