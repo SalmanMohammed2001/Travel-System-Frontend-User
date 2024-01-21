7ca9a656026146dcc2ec3c8b5e33cd4ec979eba2
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-21 21:39:34 +0530

    header create

--- src/app/module/starter/components/starter-header/starter-header.component.ts
+++ src/app/module/starter/components/starter-header/starter-header.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class StarterHeaderComponent {
 
+  hide(){
+    const box=  document.getElementById('box')
+    const show_button=  document.getElementById('show_button')
+    if(box != null && show_button !=null){
+      box.style.right='-300px'
+      box.style.transition=' all 400ms ease-in'
+      show_button.style.display='block'
+    }
+  }
+
+  show(){
+    const box=  document.getElementById('box')
+    const show_button=  document.getElementById('show_button')
+    if(box != null && show_button !=null){
+      show_button.style.display='none'
+      box.style.right='0'
+      box.style.transition=' all 400ms ease-in'
+    }
+  }
 }
