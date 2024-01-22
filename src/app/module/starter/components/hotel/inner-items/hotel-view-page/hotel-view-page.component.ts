e45ff592c499914885e016e05c7ec5a9b42a18b2
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-22 22:46:03 +0530

    hotel page view apt data set

--- src/app/module/starter/components/hotel/inner-items/hotel-view-page/hotel-view-page.component.ts
+++ src/app/module/starter/components/hotel/inner-items/hotel-view-page/hotel-view-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {HotelService} from "../../../../../../service/hotel.service";
 
 @Component({
   selector: 'app-hotel-view-page',
@@ -8,13 +9,19 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class HotelViewPageComponent implements  OnInit{
 
-  constructor(private  activateRoute:ActivatedRoute) {
+  constructor(private  activateRoute:ActivatedRoute, private service:HotelService) {
   }
+
+  hotelArrayList:Array<any>=[]
 
   ngOnInit(): void {
     this.activateRoute.paramMap.subscribe(res=>{
       let id=res.get('id')
       console.log(id)
+      this.service.findId(id).subscribe(res=>{
+        this.hotelArrayList=res.data
+        console.log(this.hotelArrayList)
+      })
     })
   }
 
